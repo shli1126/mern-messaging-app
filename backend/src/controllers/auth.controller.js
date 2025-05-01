810b4234ea7d0b8a6b79498f8e7c62fae374c33c
Shaolong Li <shaolongli26@gmail.com>
2025-04-30 17:11:25 -0700

    singup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,45 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 cahracters" });
+    }
+
+    //check is exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    //create new user
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
+  connectDB();
 });

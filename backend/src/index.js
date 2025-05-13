a8417c75bba94442b0e77f4be9bcec41310bffd8
Shaolong Li <shaolongli26@gmail.com>
2025-05-13 15:27:25 -0700

    message controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,7 @@ import {
   checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
